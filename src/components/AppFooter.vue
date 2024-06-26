02241d2f4a4bef3fdce6744a4217751d9ac9860c
valeriogonini <valeriogonini@gmail.com>
2024-06-26 16:25:52 +0200

    add foot

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,15 +1,13 @@
 <template>
     <div>
-        <p>footer</p>
+        <p>Footer</p>
     </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
