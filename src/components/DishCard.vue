6eb0129796a02cda507b709997a243cac2d500ea
Tomasz Mach <m4chtomasz@gmail.com>
2024-06-26 15:46:39 +0200

    Add: dish card

---
+++ src/components/DishCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class=" col-sm-12 col-md-6 col-xl-4 p-2">
+        <div class="card card-h p-2">
+            <h3 class="m-0">piatto: {{ dish.name }}</h3>
+            <p class="m-0">descrizzione: {{ dish.description }}</p>
+            <div>prezzo: {{ dish.price }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        dish: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.card-h {
+    height: 300px;
+}
+</style>
--- src/pages/AppRestaurant.vue
+++ src/pages/AppRestaurant.vue
@@ -1,16 +1,46 @@
 <template>
     <div>
         <RouterLink to="/">torna alla home</RouterLink>
-        <h1>Ristorante</h1>
+        <div class="container container-h">
+            <div class="row">
+                <template v-for="dish in dishes " key="dish.id">
+                    <DishCard :dish="dish"></DishCard>
+                </template>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        
+import DishCard from '../components/DishCard.vue'
+
+export default {
+    components: {
+        DishCard
+    },
+    data() {
+        return {
+            dishes: [
+                { name: 'Spagetti', description: 'Buoni spagettoni', price: '12', available: true, image: '', id: 1 },
+                { name: 'Pizza', description: 'Buona pizza', price: '8', available: true, image: '', id: 2 },
+                { name: 'Bistecca', description: 'Buona bisteccona', price: '20', available: true, image: '', id: 3 },
+                { name: 'Zuppa', description: 'Buona zuppa', price: '5', available: true, image: '', id: 4 },
+                { name: 'Piatto freddo', description: 'Piatttone buonone', price: '18', available: true, image: '', id: 5 },
+                { name: 'Piatto siciliano', description: 'Molto molto buono', price: '90', available: true, image: '', id: 6 },
+            ]
+        }
     }
+
+}
 </script>
 
 <style lang="scss" scoped>
+.container-h {
 
+    border: 1px solid black;
+}
+
+.card-h {
+    height: 300px;
+}
 </style>
