a4a7e0af4725d90ed41ef0a7be4242e7a687ac52
Tomasz Mach <m4chtomasz@gmail.com>
2024-06-26 22:45:15 +0200

    Add: button end foto to card

--- src/components/DishCard.vue
+++ src/components/DishCard.vue
@@ -1,26 +1,96 @@
 <template>
     <div class=" col-sm-12 col-md-6 col-xl-4 p-2">
-        <div class="card card-h p-2">
-            <h3 class="m-0">piatto: {{ dish.name }}</h3>
-            <p class="m-0">descrizzione: {{ dish.description }}</p>
-            <div>prezzo: {{ dish.price }}</div>
+        <div class="card">
+            <img class="card-img-top" :src="dish.image" alt="foto">
+            <div class="card-body">
+                <h3 class="m-0"> {{ dish.name }}</h3>
+                <p class="m-0">{{ dish.description }}</p>
+                <div>{{ dish.price }}$</div>
+                <button class="btn btn-primary" :class="{ 'd-none': quantity > 0 }" @click="addFood">Aggiungi al
+                    carello</button>
+                <div class="d-flex justify-content-center gap-2">
+                    <button class="custom-btn" :class="{ 'd-none': quantity < 1 }" @click="subtrack">-</button>
+                    <div :class="{ 'd-none': quantity < 1 }">{{ quantity }}</div>
+                    <button class="custom-btn" :class="{ 'd-none': quantity < 1 }" @click="add">+</button>
+                </div>
+            </div>
         </div>
     </div>
+
+
+
+
+
+
+
 </template>
 
+
+
+
+
+
+
+
+
 <script>
+
+
+
 export default {
+
     props: {
         dish: {
             type: Object,
             required: true
         }
+    },
+
+
+    data() {
+        return {
+            quantity: 0,
+        }
+    },
+
+
+    methods: {
+        addFood() {
+            this.quantity++
+            return this.quantity
+        },
+
+        add() {
+            this.quantity++
+            return this.quantity
+        },
+
+        subtrack() {
+            this.quantity--
+            return this.quantity
+        }
+
+
     }
 }
 </script>
 
 <style lang="scss" scoped>
 .card-h {
-    height: 300px;
+    height: 400px;
+}
+
+.custom-btn {
+    background: none;
+    border: none;
+    color: inherit;
+    padding: 0;
+    font: inherit;
+    cursor: pointer;
+    outline: inherit;
+}
+
+.image-h {
+    height: 150px;
 }
 </style>
--- src/pages/AppRestaurant.vue
+++ src/pages/AppRestaurant.vue
@@ -21,16 +21,12 @@ export default {
     data() {
         return {
             dishes: [
-                { name: 'Spagetti', description: 'Buoni spagettoni', price: '12', available: true, image: '', id: 10 },
-                { name: 'Pizza', description: 'Buona pizza', price: '8', available: true, image: '', id: 2 },
-                { name: 'Bistecca', description: 'Buona bisteccona', price: '20', available: true, image: '', id: 3 },
-                { name: 'Zuppa', description: 'Buona zuppa', price: '5', available: true, image: '', id: 4 },
-                { name: 'Piatto freddo', description: 'Piatttone buonone', price: '18', available: true, image: '', id: 5 },
-                { name: 'Piatto siciliano', description: 'Molto molto buono', price: '90', available: true, image: '', id: 6 },
-                { name: 'Piatto siciliano', description: 'Molto molto buono', price: '90', available: true, image: '', id: 6 },
-                { name: 'Piatto siciliano', description: 'Molto molto buono', price: '90', available: true, image: '', id: 6 },
-                { name: 'Piatto siciliano', description: 'Molto molto buono', price: '90', available: true, image: '', id: 6 },
-                { name: 'Piatto siciliano', description: 'Molto molto buono', price: '90', available: true, image: '', id: 6 },
+                { name: 'Spagetti', description: 'Buoni spagettoni', price: '12', available: true, image: 'https://www.welovepasta.it/wp-content/uploads/2017/10/Spaghetti_pomodorini.jpg', id: 1 },
+                { name: 'Pizza', description: 'Buona pizza', price: '8', available: true, image: 'https://www.welovepasta.it/wp-content/uploads/2017/10/Spaghetti_pomodorini.jpg', id: 2 },
+                { name: 'Bistecca', description: 'Buona bisteccona', price: '20', available: true, image: 'https://www.welovepasta.it/wp-content/uploads/2017/10/Spaghetti_pomodorini.jpg', id: 3 },
+                { name: 'Zuppa', description: 'Buona zuppa', price: '5', available: true, image: 'https://www.welovepasta.it/wp-content/uploads/2017/10/Spaghetti_pomodorini.jpg', id: 4 },
+                { name: 'Piatto freddo', description: 'Piatttone buonone', price: '18', available: true, image: 'https://www.welovepasta.it/wp-content/uploads/2017/10/Spaghetti_pomodorini.jpg', id: 5 },
+                { name: 'Piatto siciliano', description: 'Molto molto buono', price: '90', available: true, image: 'https://www.welovepasta.it/wp-content/uploads/2017/10/Spaghetti_pomodorini.jpg', id: 6 },
             ]
 
 
@@ -42,13 +38,4 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-.container-h {
-
-    border: 1px solid black;
-}
-
-.card-h {
-    height: 300px;
-}
-</style>
+<style lang="scss" scoped></style>
