61075ca11c5c0d2e1d9a659742811b26a446d409
valeriogonini <valeriogonini@gmail.com>
2024-06-26 16:20:29 +0200

    modifica nome ristorante

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,20 +1,20 @@
 <template>
     <div>
-        <h1  class="main">home</h1>
-        <RouterLink to="/restaurant">RESt</RouterLink>
+        <h1 class="main">home</h1>
+        <RouterLink to="/restaurant">Ristoranti</RouterLink>
     </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+
+}
 </script>
 
 <style lang="scss" scoped>
 @use '../style/partials/variables' as *;
 
-.main{
+.main {
     color: $brand-orange;
 }
 </style>
