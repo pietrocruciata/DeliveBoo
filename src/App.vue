de8c60f71fcd5fff8fb61b1ea855b807fa1c3215
LuigiRiva99 <luigi.riva.1999@gmail.com>
2024-06-26 11:58:19 +0200

    ADD: partials folder with reset and partials, added AppHeader, AppMain and AppFooter

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
-<font-awesome-icon :icon="['fas', 'magnifying-glass']" />
+  <AppHeader/>
+  <AppMain/>
+  <AppFooter/>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+
   export default {
+    components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  },
     
   }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+@use './style/general'
 
 </style>
