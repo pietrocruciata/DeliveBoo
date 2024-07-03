5d244f8d1f2188e7b066eff8e7192033460b2b84
pietrocruciata <cruciata9748@gmail.com>
2024-07-03 17:24:26 +0200

    add: section restaurant css

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,16 +1,17 @@
 <template>
-    <div class="container-lg ">
+    <div class="container ">
         <!-- <h5 class="btn-orange">L'ordine direttamente a casa tua</h5> -->
         <div>
             <h6 class="text-center my-4"><strong>Scegli per tipologia</strong></h6>
             <ul class="d-flex gap-5 justify-content-center flex-wrap">
-                <li>
-                    <!-- <div class="text-center font-icon disposition-types" @click="selectedType = [] ">
+                <!-- <li>
+                     <div class="text-center font-icon disposition-types" @click="selectedType = [] ">
                         <div class="btn-orange">tutti i ristoranti</div>
-                    </div> -->
-                </li>
-                <li class=" size-types disposition-types " v-for="(type, i) in types " :key="i"  >
-                    <div @click="filterByType(type.label, type) "  :class=" selectedType.includes(type.label)? 'btn-cream selected' : 'btn-cream'">
+                    </div> 
+                </li> -->
+                <li class=" size-types disposition-types col-xl-2" v-for="(type, i) in types " :key="i">
+                    <div @click="filterByType(type.label, type)"
+                        :class="selectedType.includes(type.label) ? 'btn-cream selected' : 'btn-cream'">
                         <img :src="type.image" alt="" class="text-center font-icon">
                         <div>{{ type.label }}</div>
                     </div>
@@ -18,36 +19,51 @@
             </ul>
 
         </div>
-        <div class="my-4 row">
-            <!-- <RouterLink to="/restaurant" class="text-center"><strong>Tutti i nostri Ristoranti</strong></RouterLink> -->
-            <div v-if="filteredRestaurants.length === 0">
-                <p>Nessun ristorante corrispondente.</p>
-            </div>
-            <div v-else class="my-4 row">
-                    <div v-for="(restaurant, index) in filteredRestaurants" :key="index" class="col-sm-12 col-md-6 col-xl-4 p-2" >
-                        <div class="card">
-                            <h3>{{ restaurant.name }}</h3>
-                            <p>{{ restaurant.description }}</p>
-                            <p><strong>Indirizzo:</strong> {{ restaurant.address }}</p>
-                            <p><strong>Email:</strong> {{ restaurant.email }}</p>
-                            <p><strong>Partita IVA:</strong> {{ restaurant.p_iva }}</p>
-                            <div>
-                                <strong>Tipi:</strong>
-                                <ul>
-                                    <li v-for="(type, j) in restaurant.types" :key="j">
-                                    <img :src="type.image" alt="" class="text-center font-icon">
-                                    {{ type.label }}
-                                    </li>
-                                </ul>
-                            </div>
+    </div>
 
-                        </div>
+    <section>
+        <div class="container">
+            <div class="my-4 row ">
+                <strong class="text-center">Tutti i nostri Ristoranti</strong>
+                <RouterLink to="/restaurant">
+                    <div v-if="filteredRestaurants.length === 0">
+                        <p>Nessun ristorante corrispondente.</p>
                     </div>
+                    <div v-else class="my-4 row">
+
+                        <div v-for="(restaurant, index) in filteredRestaurants" :key="index"
+                            class="col-sm-12 col-md-6 col-xl-4 p-2">
+                            <div class="card">
+                                <img :src="restaurant.image" alt="">
+                                <h3>{{ restaurant.name }}</h3>
+                                <p>{{ restaurant.description }}</p>
+                                <p><strong>Indirizzo:</strong> {{ restaurant.address }}</p>
+                                <p><strong>Email:</strong> {{ restaurant.email }}</p>
+                                <p><strong>Partita IVA:</strong> {{ restaurant.p_iva }}</p>
+                                <div>
+                                    <strong>Tipi:</strong>
+                                    <ul>
+                                        <li v-for="(type, j) in restaurant.types" :key="j">
+                                            <img :src="type.image" alt="" class="text-center font-icon">
+                                            {{ type.label }}
+                                        </li>
+                                    </ul>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </RouterLink>
             </div>
         </div>
+    </section>
 
 
-    </div>
+
+
+
+
 </template>
 
 <script>
@@ -58,7 +74,7 @@ export default {
             restaurants: [],
             types: [],
             selectedType: [],
-          
+
         }
     },
 
@@ -66,7 +82,7 @@ export default {
 
         fetchRestaurants() {
             //chiamata axios
-            axios.get('http://127.0.0.1:8000/api/restaurants',{
+            axios.get('http://127.0.0.1:8000/api/restaurants', {
                 // params: {
                 // perPage : this.perPage,
                 // page: this.currentPage
@@ -83,7 +99,7 @@ export default {
 
         fetchTypes() {
             //chiamata axios
-            axios.get('http://127.0.0.1:8000/api/types',{
+            axios.get('http://127.0.0.1:8000/api/types', {
                 // params: {
                 // perPage : this.perPage,
                 // page: this.currentPage
@@ -93,7 +109,7 @@ export default {
 
                 this.types = res.data.types
 
-                console.log(this.types);
+                // console.log(this.types);
                 // this.lastPage = res.data.restaurants.last_page
             })
         },
@@ -101,19 +117,19 @@ export default {
         filterByType(typeLabel, i) {
             const index = this.selectedType.indexOf(typeLabel);
             if (index === -1) {
-                this.selectedType.push(typeLabel);  
-                console.log(this.selectedType);
+                this.selectedType.push(typeLabel);
+                // console.log(this.selectedType);
             } else {
-                this.selectedType.splice(index, 1); 
-                console.log(this.selectedType);
+                this.selectedType.splice(index, 1);
+                // console.log(this.selectedType);
             }
-          
+
         }
     },
 
-    created(){
-      this.fetchRestaurants(),
-      this.fetchTypes()
+    created() {
+        this.fetchRestaurants(),
+            this.fetchTypes()
     },
 
     computed: {
@@ -122,7 +138,7 @@ export default {
                 return this.restaurants;
             } else {
                 return this.restaurants.filter(restaurant => {
-                return restaurant.types.some(type => this.selectedType.includes(type.label));
+                    return restaurant.types.some(type => this.selectedType.includes(type.label));
                 });
             }
         }
@@ -135,37 +151,38 @@ export default {
 <style lang="scss" scoped>
 @use '../style/partials/variables' as *;
 
-    .main {
-        color: $brand-orange;
-    }
+.main {
+    color: $brand-orange;
+}
 
-    .btn-cream {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        border-radius: 20px;
-        border: 1px solid black;
-        background-color: #F4EFE7;
-        width: 110px;
-        padding: 10px;
-        cursor: pointer;
-    }
+.btn-cream {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-radius: 20px;
+    // border: 1px solid black;
+    background-color: #F4EFE7;
+    width: 110px;
+    padding: 10px;
+    cursor: pointer;
 
-    .font-icon {
-        width: 40px;
-    }
+}
 
-    .size-types {
-        width: 120px;
-    }
+.font-icon {
+    width: 40px;
+}
 
-    .disposition-types {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
+// .size-types {
+//     width: 120px;
+// }
 
-    .selected{
-        background-color: $brand-orange;
-    }
+.disposition-types {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.selected {
+    background-color: $brand-orange;
+}
 </style>
